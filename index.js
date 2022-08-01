95d842b11d3fb4562ae5d0e1b6dee0edfe495717
rayannepinto <ray.rps18@gmail.com>
2022-08-01 11:53:35 -0300

    Algumas últimas alterações

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const buscarNumero = () => {
 };
 
 
+function cartaoValidacao () {
+    if (creditCardNumber === "") {
+        document.getElementById("resultado").innerText = "*Campo não pode estar vazio!"
+    } else if (isNaN(creditCardNumber)){
+        document.getElementById("resultado").innerText = "Digite apenas números!"
+    } else if (validator.isValid(creditCardNumber) === false) {
+        document.getElementById("resultado").innerText = "Cartão inválido! Tente novamente."
+        document.getElementById("botaoReserva").style.backgroundColor = "gray";
+    } else if (validator.isValid(creditCardNumber) === true)
+    {
+        document.getElementById("resultado").innerText = "Seu cartão é válido!"
+        document.getElementById("botaoReserva").style.backgroundColor = "green";
+    }
+}
+
 const mascarandoNumero = () => {
     let novoNumeroCartao = validator.maskify(creditCardNumber);
     let numeroTransformado = document.getElementById("numero-cartao");
@@ -19,27 +34,11 @@ const mascarandoNumero = () => {
     return(document.getElementById("numero-cartao").value = novoNumeroCartao);
 };
 
-
-function cartaoValidacao () {
-    if (creditCardNumber === "" || creditCardNumber === null) {
-        document.getElementById("resultado").innerHTML = "*Campo não pode estar vazio!"
-    } else if (isNaN(creditCardNumber)){
-        document.getElementById("resultado").innerHTML = "Digite apenas números!"
-    } else if (validator.isValid(creditCardNumber) === false) {
-        document.getElementById("resultado").innerHTML = "Cartão inválido! Tente novamente."
-        document.getElementById("botaoReserva").style.backgroundColor = "gray";
-    } else if (validator.isValid(creditCardNumber) === true)
-    {
-        document.getElementById("resultado").innerHTML = "Seu cartão é válido!"
-        document.getElementById("botaoReserva").style.backgroundColor = "green";
-    }
-}
-
 let creditCardNumber = "";
-let caixaDoCartao = document.getElementById("numero-cartao");
-caixaDoCartao.addEventListener("change", buscarNumero);
-caixaDoCartao.addEventListener("change", () => validator.maskify(creditCardNumber));
-caixaDoCartao.addEventListener("change", mascarandoNumero);
+let chamadaNumeroCartao = document.getElementById("numero-cartao");
+chamadaNumeroCartao.addEventListener("change", buscarNumero);
+chamadaNumeroCartao.addEventListener("change", () => validator.maskify(creditCardNumber));
+chamadaNumeroCartao.addEventListener("change", mascarandoNumero);
 
 let botaoVerificar = document.getElementById("botao-verificar");
     botaoVerificar.addEventListener("click", () => {
